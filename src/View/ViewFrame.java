af67dfd9a1fdac37540e8b30b1a1dd6b7e220e40
rezaazim9 <ostadrezaazim@gmail.com>
2024-06-05 02:27:54 +0330

    upload

--- src/View/ViewFrame.java
+++ src/View/ViewFrame.java
@@ -4,10 +4,8 @@ import Model.Account;
 import Model.RFile;
 
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.io.File;
 import java.util.ArrayList;
 
 public class ViewFrame implements ActionListener {
